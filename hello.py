035204d5371cb699d641673fdc93fe3b83dd476c
David J Kim <jhukdk@gmail.com>
2026-01-06 17:17:40 -0500

    this is a new change a 517 pm

--- hello.py
+++ hello.py
@@ -8,3 +8,5 @@ print(len(nameVar))
 print("How old are you?")
 ageVar = input()
 print("You will be " + str(int(ageVar) + 1) + " next year.")
+
+print("This is a new change on 01.06.2025 at 5:17 pm")
