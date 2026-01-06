a7ff56a511c46f6a0c6454f31f485e3d4b5858ab
David J Kim <jhukdk@gmail.com>
2026-01-06 16:54:06 -0500

    new commit

--- hello.py
+++ hello.py
@@ -2,3 +2,9 @@ print("What is your name?")
 nameVar = input()
 print("It is nice to meet you, " + nameVar)
 
+print("The length of your name is:")
+print(len(nameVar))
+
+print("How old are you?")
+ageVar = input()
+print("You will be " + str(int(ageVar) + 1) + " next year.")
