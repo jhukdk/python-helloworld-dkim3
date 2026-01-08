402f43b75fe834a93e8ad937f124df11827beefa
David J Kim <jhukdk@gmail.com>
2026-01-07 22:48:14 -0500

    added 2 new files from automateboringstuff

---
+++ additionforloop.py
@@ -0,0 +1,10 @@
+"""
+currentsum = 0
+for i in range(100):
+    currentsum = currentsum + i
+    print(currentsum)
+"""
+
+whatis = ((True or False) and False)    
+print(whatis)
+
---
+++ jeremy.py
@@ -0,0 +1,10 @@
+print("What is your name?")
+dummy = input() 
+dummy = dummy.lower()
+
+if dummy == "jeremy":
+    print("Hello dummy!")
+elif dummy == "david":
+    print("You're a genius! Very unlike that dummy Jeremy guy")
+else:
+    print("You are neither bignog nor junglep.")
